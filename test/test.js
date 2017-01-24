0af8c91fdee9f75cf56eba1b6581220fc3694205
Mel Booth <mel.m.booth@gmail.com>
2017-01-25 11:25:15 +1300

    start writing reducer tests - coffeeeee break

--- test/test.js
+++ test/test.js
@@ -1,9 +1,18 @@
 import test from 'tape'
 import freeze from 'deep-freeze'
 
+import reducer from '../reducer'
+
 
 test('ADD_TO_CART adds one item to shopping cart', function (t) {
-  t.ok(true)
+  var initialState = {
+    cart: {
+
+    }
+  }
+  freeze(initialState)
+  var newState = reducer(initialState, {type: 'ADD_TO_CART', payload: 1})
+  t.equal(newState.cart[1], 1)
   t.end()
 })
 
